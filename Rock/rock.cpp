3964599359dd6aace866cf70ff82c6d1e684c3d7
Robyn Doyle <robyn.doyle@gmail.com>
2024-06-21 10:23:19 +0100

    Wrote user input side of rock game

--- Rock/rock.cpp
+++ Rock/rock.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 using namespace std;
+
+int choose(int choice);
+
+
 int main(){
 
-    cout << "Rock Paper Scissors";
+    int menu;
+    int choice;
+
+    cout << "Choose 1 or 2\n 1 = Play game\n 2 = Exit\n" << endl; 
+    cin >> menu;
+
+    switch(menu){
+        case 1: choose(choice);
+            break;
+        case 2: cout << "Thanks for visiting!\n";
+            break;
+    }
+
+    //cout << choice << endl;
+    return 0;
+
+    
 
 }
+
+
+int choose(int choice){
+
+    
+
+    cout << "Choose 1, 2 or 3: 1 = Rock, 2 = Paper, 3 = Scissors" << endl; 
+    cin >> choice;
+    
+     if(choice == 1 || choice == 2 || choice == 3){
+        
+        switch(choice){
+            case 1: cout << "Nice, you chose rock\n";
+                break;
+            case 2: cout << "Nice, you chose paper\n";
+                break;
+            case 3: cout << "Nice, you chose scissors\n";
+                break;          
+        }
+        return choice;
+    }   
+    else {
+        cout << "Oh no, you need to enter 1 2 or 3\n";
+        return 0;
+    }
+
+}
+
+// User choice shoose 1 2 or 3
+// computer choice wil be srand(time NULL) % 3 -1 
