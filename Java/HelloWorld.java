5ef35e38b368053a6a87be6ba89fc777aad4d9be
Robyn Doyle <robyn.doyle@gmail.com>
2024-07-10 18:13:52 +0100

    Created first Java files and objects

---
+++ Java/HelloWorld.java
@@ -0,0 +1,18 @@
+// Define the class
+public class HelloWorld {
+    // The main method: entry point of the application
+    public static void main(String[] args) {
+        // // Print a message to the console
+        System.out.println("Hello, World!");
+        test("Tom");
+
+        Name.Tom();
+    }
+
+
+    private static void test(String name) {
+        System.out.println("FRENCH fries for " + name);
+    }
+}
+
+
---
+++ Java/Name.java
@@ -0,0 +1,5 @@
+public class Name {
+    public static void Tom() {
+        System.out.println("His name is Tom! ");
+    }
+}
