725a5fe066fddbe3339ad8fdde2cae663bfb5646
Robyn Doyle <robyn.doyle@gmail.com>
2024-07-12 10:29:45 +0100

    Select driver page linked and initail set up

--- F1_Fantasy_Game/F1_build/app.py
+++ F1_Fantasy_Game/F1_build/app.py
@@ -15,6 +15,10 @@ def home():
 def start():
     return render_template('start.html')
 
+@app.route('/select_driver', methods=['GET', 'POST'])
+def select_driver():
+    return render_template('select_driver.html')
+
 # app.run(host="0.0.0.0", port=80)
 
 if __name__ == '__main__':
